6531d607e3ca646dc52fc27b5f0db664440617e8
Yatten <cosmoreddog@gmail.com>
2023-04-23 16:53:34 +0300

    Updated Modal component and setting up effects for it

--- src/pages/Modal.js
+++ src/pages/Modal.js
@@ -1,7 +1,7 @@
 import React  from "react";
 
 
-let Modal = ({title, image, dateRelease, overview}) => {
+let Modal = ({image, dateRelease, overview, vote_average}) => {
     let [showModal, setShowModal] = React.useState(false)
 
     return (
@@ -14,15 +14,13 @@ let Modal = ({title, image, dateRelease, overview}) => {
 
             {showModal ? (
                 <>
-                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                        <div className="relative w-auto my-6 mx-auto max-w-sm">
+                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none backdrop-blur">
+                        <div className="relative w-auto my-6 mx-auto max-w-sm ">
                             {/* content */}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
-                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                    <h3 className="text-3xl font-semibold">
-                                        {title}
-                                    </h3>
+                                <div className="flex items-start justify-between p-1 border-b border-solid border-slate-200 rounded-t">
+
                                     <button
                                         className="p-1 ml-auto bg-transparent border-0 text-black opacity-100 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                                         onClick={() => setShowModal(false)}
@@ -35,12 +33,17 @@ let Modal = ({title, image, dateRelease, overview}) => {
                                 </div>
                                 {/*body*/}
                                 <div className=" justify-center relative p-6 flex-auto">
-                                    <img className="flex object-scale-down h-48 w-96 ..." src = {image} alt=""/>
-                                    <h3 className=" flex justify-center text-2xl font-semibold">Release date: {dateRelease}</h3>
-                                    <p className="my-4 text-slate-500 text-lg leading-relaxed">{overview}</p>
+                                    <img className="flex  ..." src = {image} alt=""/>
+                                    <h4 className="font-bold text-2xl">IMDb: {vote_average}</h4>
+                                    <h5 className=" flex text-2xl font-semibold">Release date: {dateRelease}</h5>
+                                    <br></br>
+                                    <h6 className="font-bold text-lg leading-relaxed">Overview</h6>
+                                    <p className="text-slate-500 text-lg leading-relaxed">{overview}</p>
+
                                 </div>
+
                                 {/*footer*/}
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                <div className="flex items-center justify-end p-2 border-t border-solid border-slate-200 rounded-b">
                                     <button
                                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
--- src/pages/MovieBox.js
+++ src/pages/MovieBox.js
@@ -4,14 +4,14 @@ import Modal from "./Modal";
 
 let API_IMG = "https://image.tmdb.org/t/p/w500/";
 
-let MovieBox = ({title, poster_path, release_date, overview}) => {
+let MovieBox = ({title, poster_path, release_date, overview, vote_average}) => {
     return (
         <div className="w-full p-10 shadow-md lg:max-w-lg items-center">
             <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" >
                 <img className="h-auto max-w-full rounded-lg" src ={API_IMG + poster_path} alt=""/>
             </div>
             <div className="flex justify-center">
-                <Modal title = {title} image = {API_IMG + poster_path} dateRelease={release_date} overview={overview}/>
+                <Modal title = {title} image = {API_IMG + poster_path} dateRelease={release_date} overview={overview} vote_average={vote_average}/>
             </div>
             {/* <h1>{title}</h1>
             <h2>{release_date}</h2>
